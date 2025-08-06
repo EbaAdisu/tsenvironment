86d7019ae271ed40b8672ec2d77137b0b913c00c
Eba Adisu <EbaAdisuKenea>
2025-08-07 01:02:38 +0300

    feat: simplify home page for Vercel debugging

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import { Hero } from '@/components/hero'
-import { Projects } from '@/components/projects'
-import { Services } from '@/components/services'
-
 export default function HomePage() {
-    return (
-        <main>
-            <Hero />
-            <Services />
-            <Projects />
-        </main>
-    )
+  return (
+    <div>
+      <h1>Hello Vercel!</h1>
+    </div>
+  );
 }
